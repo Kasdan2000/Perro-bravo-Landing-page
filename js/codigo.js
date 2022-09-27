7852f7cd01f2b42a9faea381346c9d3e4f7cdd7f
Lucas Martínez <77300514+Kasdan2000@users.noreply.github.com>
2022-09-27 16:35:59 -0300

    Primera subida de archivos 75%

---
+++ js/codigo.js
@@ -0,0 +1,105 @@
+let botonDeAperturaDeForm = document.querySelector("h1 + div");
+let nombreDelCliente = document.querySelector("#nombreDelCliente");
+let tipoDeEmpresa = document.querySelector("#tipoDeEmpresa");
+let rolDelCliente = document.querySelector("#rolDelCliente"); 
+let procedencia = document.querySelector("#procedencia");
+let rubro = document.querySelector("#rubro");
+let nroTelefono = document.querySelector("#nroTelefono");
+let email = document.querySelector("#email");
+let presupuesto = document.querySelector("#presupuesto");
+let descripcion = document.querySelector("#descripcion");
+let fechaDeComienzo = document.querySelector("#fechaDeComienzo");
+let fechaDeFinalización = document.querySelector("#fechaDeFinalizacion");
+let btnEnviar = document.querySelector("#btnEnviar");
+let menuHum = document.querySelector("#menuHum");
+let menuHum2 = document.querySelector("#menuHum2");
+let claseMenuHum = document.querySelector(".menuHum");
+let menuDesplegable = document.querySelector("#menuDesplegable");
+let accesoSeccionAcerca = document.querySelector("#abrirAcerca");
+let accesoSeccionContacto = document.querySelector("#abrirContacto");
+
+cargarSeelectPresupuestos();
+
+function cargarSeelectPresupuestos() {
+    for(let i = 0; i <= presupuestos.length - 1; i++) {
+        presupuesto.innerHTML += `<option value="${presupuestos[i]}"
+        >${presupuestos[i]}</option>`
+
+    }
+}
+
+menuHum.addEventListener("click", abrirMenuHum);
+menuHum2.addEventListener("click", abrirMenuHum);
+
+function abrirMenuHum(){
+    const visibility = menuDesplegable.getAttribute("data-visible");
+    if(visibility === "false"){
+        menuDesplegable.setAttribute("data-visible", true);
+    }else if(visibility === "true"){
+        menuDesplegable.setAttribute("data-visible", false);
+    }
+}
+
+
+accesoSeccionAcerca.addEventListener("click", abrirSeccionAcerca);
+
+function abrirSeccionAcerca() {
+    const visibility = menuDesplegable.getAttribute("data-visible");
+    if(visibility === "true"){
+        menuDesplegable.setAttribute("data-visible", false);
+        document.querySelector("#seccionAcerca").style.display = "block";
+        document.querySelector("h1").style.display = "none";
+        document.querySelector("h1 + div").style.display = "none";
+        document.querySelector("#formularioDeProyecto").style.display = "none";
+    }
+    else{
+        document.querySelector("#seccionAcerca").style.display = "block";
+        document.querySelector("h1").style.display = "none";
+        document.querySelector("h1 + div").style.display = "none";
+        document.querySelector("#formularioDeProyecto").style.display = "none";
+    }
+}
+
+
+botonDeAperturaDeForm.addEventListener("click", abrirSectionContacto);
+accesoSeccionContacto.addEventListener("click", abrirSectionContacto);
+
+function abrirSectionContacto(){
+    const visibility = menuDesplegable.getAttribute("data-visible");
+  /*   let h1 = document.querySelector("h1");
+    let sectionAcerca = document.querySelector("#seccionAcerca");
+    let form = document.querySelector("#formularioDeProyecto"); */
+    let vw = window.innerWidth;
+    if(vw <= 767){
+        if(visibility === "true"){
+            menuDesplegable.setAttribute("data-visible", false);
+            document.querySelector("#seccionAcerca").style.display = "none";
+            document.querySelector("h1").style.display = "block";
+            document.querySelector("h1 + div").style.display = "block";
+            document.querySelector("#formularioDeProyecto").style.display = "block";    
+        }
+         else{
+            document.querySelector("#seccionAcerca").style.display = "none";
+            document.querySelector("h1").style.display = "block";
+            document.querySelector("h1 + div").style.display = "block";
+            document.querySelector("#formularioDeProyecto").style.display = "block";
+         } 
+    }
+    if(vw >= 768){
+        if(visibility === "true"){
+            menuDesplegable.setAttribute("data-visible", false);
+            document.querySelector("#seccionAcerca").style.display = "none";
+            document.querySelector("h1").style.display = "block";
+            document.querySelector("h1 + div").style.display = "block";
+            document.querySelector("#formularioDeProyecto").style.display = "grid";    
+        }
+        else{
+            document.querySelector("#seccionAcerca").style.display = "none";
+            document.querySelector("h1").style.display = "block";
+            document.querySelector("h1 + div").style.display = "block";
+            document.querySelector("#formularioDeProyecto").style.display = "grid";
+        }
+    }
+}
+
+
